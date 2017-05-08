90391df974f5e8033ae79daa5fc7a9a8f5577938
Emil Lykke Jensen <emil@emillykkejensen.dk>
2017-05-08 17:45:40 +0200

    Added options removePunctuation and removeURLs

--- R/textfunc_textcleaner.R
+++ R/textfunc_textcleaner.R
@@ -1,21 +1,27 @@
 #' Clean SoMe text
 #'
 #' @param textdata character vector; The text you want cleaned
+#' @param removePunctuation logical; If TRUE (default) it wil remove all punctuation and symbols from text
+#' @param removeURLs logical; If TRUE (default) it wil remove all URLs from text
 #' @param removeRT logical; If TRUE it wil remove 'RT' and 'via' from text
 #' @param removeHashtag logical; If TRUE it wil remove all words begining with '#' from text
 #' @param removeHandles logical; If TRUE it wil remove all words begining with '@' from text
 #' @return Character string
 #' @export
 
-textfunc.textcleaner <- function(textdata, removeRT = FALSE, removeHashtag = FALSE, removeHandles = FALSE){
+textfunc.textcleaner <- function(textdata,
+                                 removePunctuation = TRUE,
+                                 removeURLs = TRUE,
+                                 removeRT = FALSE,
+                                 removeHashtag = FALSE,
+                                 removeHandles = FALSE){
 
   if(removeRT) textdata <- gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", textdata)
   if(removeHashtag) textdata <- gsub("[#]\\S+", "", textdata)
   if(removeHandles) textdata <- gsub("[@]\\S+", "", textdata)
-
-  textdata <- gsub("http://t.co/[a-z,A-Z,0-9]*{8}", "", textdata)
-  textdata <- gsub("https://t.co/[a-z,A-Z,0-9]*{8}", "", textdata)
-  textdata <- gsub("[[:punct:]]+", "", textdata)
+  if(removeURLs) textdata <- gsub("http://t.co/[a-z,A-Z,0-9]*{8}", "", textdata)
+  if(removeURLs) textdata <- gsub("https://t.co/[a-z,A-Z,0-9]*{8}", "", textdata)
+  if(removePunctuation) textdata <- gsub("[[:punct:]]+", "", textdata)
   textdata <- tolower(textdata)
   textdata <- trimws(textdata)
 
